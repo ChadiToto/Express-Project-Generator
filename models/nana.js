0eb4b0fac6c3f82ac2906254f48daeac89230dc4
ChadiToto <chadi.toto@insa-cvl.fr>
2020-09-30 21:57:58 +0200

    removed some test files

--- models/nana.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-var schemaName = new Schema({ fields });
-
-var modelName = mongoose.Model(modelName, schemaName);
-
-module.exports = { modelName, schemaName };
